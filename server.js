c9e3170e39849cdf59d7af5f0ebb6019752e0871
francinedp <francined18@gmail.com>
2019-07-10 21:17:15 -0300

    aula 12

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+
+app.listen(port);
